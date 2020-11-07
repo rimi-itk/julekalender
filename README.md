b4c00a0fb5503999b401de9fd9baba3d3e51defb
Mikkel Ricky <rimi@aarhus.dk>
2020-11-07 08:24:02 +0100

    003

---
+++ README.md
@@ -0,0 +1,7 @@
+# Julekalender
+
+```sh
+composer install
+bin/console doctrine:migrations:migrate --no-interaction
+yarn build
+```
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("", name="frontpage")
+     */
+    public function index(): Response
+    {
+        return $this->redirectToRoute('admin');
+    }
+}
